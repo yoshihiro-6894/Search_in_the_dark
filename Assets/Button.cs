a2626dc32fb7c8bd1473517fd9423ad079832a46
Haru-piano <72134544+Haru-piano@users.noreply.github.com>
2021-03-12 00:18:47 +0900

    ボタンを追加しましたが、エラーになってしまいました
    ボタンを追加して、PreStage1というTextを追加しましたが、All compiler errors have to be fixed before you can enter playmode! というエラーが出てしまいました

---
+++ Assets/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; //追加しましょう
+
+public class Button : MonoBehaviour
+{
+    public GameObject score_object = null; //textオブジェクト
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.Getcomponent<Text>();
+
+        //テキストの表示を入れ替える
+        score_text.text = "PreStage1";
+    }
+}
