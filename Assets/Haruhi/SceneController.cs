c2c9580704974512047afe8e76dab82fb0aa72de
Haru-piano <72134544+Haru-piano@users.noreply.github.com>
2021-03-16 16:35:06 +0900

    タイトル画面を作りました
    タイトル画面とボタンはできたのですが、シーンを移動することができません

---
+++ Assets/Haruhi/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ボタンを使用するためUIとSceneManagerを使用するためSceneManagementを追加
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    // ボタンをクリックするとPreStage1に移動する
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("PreStage1");
+    }
+}
