759df4a2e16df077ff1a47993d748d80efceee1d
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2021-03-23 14:11:16 +0900

    make_stage

--- Assets/Yossy/Scripts/Goal.cs
+++ Assets/Yossy/Scripts/Goal.cs
@@ -21,7 +21,7 @@ public class Goal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;//ゴールを複数回触れることを防ぐ
         goalAnim.SetBool("isGet", true);
     }
 }
