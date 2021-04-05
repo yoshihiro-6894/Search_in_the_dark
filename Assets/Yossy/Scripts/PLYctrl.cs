54cb969bfcef22c1a79d4b2ff2f066d36dd1f827
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2021-04-06 04:59:46 +0900

    CleanUp_folder
    Because there are more animations

--- Assets/Yossy/Scripts/PLYctrl.cs
+++ Assets/Yossy/Scripts/PLYctrl.cs
@@ -11,11 +11,12 @@ public class PLYctrl : MonoBehaviour
     [SerializeField] private float jumpForce = 650.0f;//[SerializeField]によってUnityEditor上で編集できる
     [SerializeField] private float walkForce = 7.0f;
     [SerializeField] private float MaxjumpHeight = 2.5f;
+    [SerializeField] ContactFilter2D filter2d;
 
     private bool Isjump = false;
     private bool onGround = false;
 
-    float key;
+    float key;//左右移動-1,0.1をとる
 
 
     // Start is called before the first frame update
@@ -24,47 +25,36 @@ public class PLYctrl : MonoBehaviour
         this.rigid2D = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
         this.sprite = GetComponent<SpriteRenderer>();
-        
+        if (sprite.sprite.name == "right1")
+            animator.SetBool("startidle", true);
+        else
+            animator.SetBool("startidle", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) && this.rigid2D.velocity.y == 0)
-        {
-            this.rigid2D.AddForce(transform.up * this.jumpForce);
-        }
-        /*
-        int key = 0;
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            key = 1;
-            this.animator.SetTrigger("RightTrigger");
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            key = -1;
-            this.animator.SetTrigger("LeftTrigger");
-        }
-       */
-        key = Input.GetAxisRaw("Horizontal");
+        key = Input.GetAxisRaw("Horizontal");//左-1,右1,その他0
         animator.SetFloat("Xvec", key);
-        /*
-        if(key>0)
-            this.animator.SetTrigger("RightTrigger");
-        if (key < 0)
-            this.animator.SetTrigger("LeftTrigger");
-        */
 
-        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+        onGround = rigid2D.IsTouching(filter2d);
 
-        //rigid2D.velocity = new Vector2(key * walkForce, rigid2D.velocity.y);
+        if (onGround)
+        {
+            if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+        }
     }
     private void FixedUpdate()
     {
         rigid2D.velocity = new Vector2(key * walkForce, rigid2D.velocity.y);
-        //this.rigid2D.AddForce(transform.up * this.jumpForce);
+        if (Isjump)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+            Isjump = false;
+        }
     }
 
     private void Jump()
