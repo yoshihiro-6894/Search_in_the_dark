a5d422cc4216fd20c76ac675919e4ca13baad296
Ashiheart <extrmediamond@yahoo.co.jp>
2021-04-08 23:08:25 +0900

    Update DarknessBehaviour.cs

--- Assets/Ashiheart/DarknessBehaviour.cs
+++ Assets/Ashiheart/DarknessBehaviour.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DarknessBehaviour : MonoBehaviour
+[RequireComponent(typeof(BoxCollider2D))]
+public abstract class DarknessBehaviour : MonoBehaviour
 {
+    public abstract void Illminated();
+
+    public abstract void Disillminated();
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
