dafaecf8cf8d78f21b6e9998d9ccbf99321afe2d
Ashiheart <extrmediamond@yahoo.co.jp>
2021-04-19 18:32:49 +0900

    clean up folders

---
+++ Assets/Ashiheart/SearchLights/GhostBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+[RequireComponent(typeof(Collider2D))]
+
+public class GhostBlock : MonoBehaviour, IDarknessBehaviour
+{
+    public bool onLighted { get; set; }
+
+    public void LightEnter(bool characterEnter)
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = characterEnter;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.OnTriggerEnter2DAsObservable()
+            .Where(c => string.Equals(c.gameObject.name, "Character"))
+            .Subscribe(c => gameObject.GetComponent<Collider2D>().isTrigger = false)
+            .AddTo(this)
+            ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
