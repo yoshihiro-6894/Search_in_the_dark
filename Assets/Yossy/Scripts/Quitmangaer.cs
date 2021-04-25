d0051b51696ccdf1f90a4566ebf10e97ea04435a
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2021-04-25 16:46:27 +0900

    add font

---
+++ Assets/Yossy/Scripts/Quitmangaer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitmangaer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EscapeQuit();
+        }
+    }
+
+    void EscapeQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+}
