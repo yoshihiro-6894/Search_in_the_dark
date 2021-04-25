75bca6fbe27c7c42cef3babe6e466cec44b8e97f
Ashiheart <extrmediamond@yahoo.co.jp>
2021-04-25 16:21:57 +0900

    StageSelector

---
+++ Assets/Ashiheart/StageSelect/MoveSceneButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveSceneButton : MonoBehaviour
+{
+    [SerializeField] Scene scene;
+
+    [SerializeField] Color color;
+
+    [SerializeField] string description;
+
+    private Text difficulty;
+
+    private Color default_color;
+
+    private string default_description;
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene(scene.handle);
+    }
+
+    public void OnButtonHighlighted()
+    {
+        Debug.Log(description);
+
+        difficulty.color = color;
+        difficulty.text = description;
+    }
+
+    public void OnButtonDishighlighted()
+    {
+        difficulty.color = Color.white;
+        difficulty.text = ""; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difficulty = GameObject.Find("Difficulty").GetComponent<Text>();
+
+        default_color = new Color(
+            difficulty.color.r,
+            difficulty.color.g,
+            difficulty.color.b
+            );
+
+        default_description = string.Copy(difficulty.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ashiheart/StageSelect/StageDiscription.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageDescription : MonoBehaviour
+{
+    [SerializeField] private List<Color> c;
+
+    [SerializeField] private List<string> d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
